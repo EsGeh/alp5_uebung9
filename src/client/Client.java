2c83915768d1bb50f3d0434df171b02d1c2b261d
Samuel Gfrörer <SamuelGfroerer@googlemail.com>
2014-02-11 19:32:45 +0100

    client memorizes session-ids now

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,27 @@
 package client;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.rmi.AccessException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+
+import sun.org.mozilla.javascript.ast.TryStatement;
 
 import common.BrowserGenerator;
 import common.Counter;
@@ -47,8 +64,13 @@ public class Client {
 
 		int sessionID = 0;
 		{
-			SessionGenerator sessionGen = (SessionGenerator )reg.lookup("SessionGenerator");
-			sessionID = sessionGen.getSessionID();
+			try {
+				sessionID = fetchSessionID(reg, appName);
+			}
+			catch(IOException e) {
+				System.out.println("error while reading from file: " + e.getMessage());
+				return;
+			}
 			System.out.println("sessionID: " + sessionID);
 		}
 		//Data data = (Data )reg.lookup("Counter");
@@ -67,10 +89,59 @@ public class Client {
 		}
 		catch(IncorrectSessionID e) {
 			System.out.println("invalid session id: " + e.getMessage());
+			return;
 		}
 		browser.init(data);
 		browser.show();
 	}
+	
+	private int fetchSessionID( Registry reg, String appName ) throws IOException, NotBoundException {
+		int i = tryToLoad( appName );
+		if( i == -1 ){
+			SessionGenerator sessionGen = (SessionGenerator )reg.lookup("SessionGenerator");
+			i = sessionGen.getSessionID();
+			
+			// memorize sessionID for future sessions:
+			System.out.println("memorizing cookie for future sessions...");
+			createCookie(appName, i);
+		}
+		return i;
+	}
+	
+	private int tryToLoad( String appName ) throws IOException {
+		int i = -1;
+		Path path = Paths.get(cookieDir + "/" + appName);
+		if( Files.exists(path)) {
+			System.out.println("cookie found for " + appName);
+			try(
+				InputStream is = new FileInputStream( path.toString() ); 
+				InputStream bufferedIS = new BufferedInputStream(is);
+				ObjectInput input = new ObjectInputStream (bufferedIS);
+				//input.close();
+			)
+			{
+				i = (Integer )input.readObject();
+			}
+			catch (ClassNotFoundException e) {
+				throw new IOException("error while loading a cookie: " + e.getMessage());
+			}
+		}
+		return i;
+	}
+	
+	private void createCookie( String appName, int sessionID ) throws IOException {
+		Path path = Paths.get(cookieDir);
+		Files.createDirectories( path ); // should create the cookies-dir, if not existing
+		OutputStream file = new FileOutputStream(cookieDir + "/" + appName);
+		OutputStream buffer = new BufferedOutputStream(file);
+		ObjectOutput output = new ObjectOutputStream(buffer);
+		output.writeObject( new Integer(sessionID) );
+		output.close();
+		System.out.println("cookie created!");
+	}
+	
+	private static String appName = "Counter";
+	private static String cookieDir = "cookies";
 	
 	private String host;
 	private int port;
--- src/server/CounterBrowser.java
+++ src/server/CounterBrowser.java
@@ -15,32 +15,11 @@ public class CounterBrowser implements GUI<Counter> {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	/*public CounterBrowser(Counter data) {
-		//counter = data;
-	}*/
-
 	@Override
 	public void show() {
 		System.out.println("CounterBrowser.show()");
 		CounterFrame frame = new CounterFrame(counter);
 		frame.setVisible(true);
-		/*Frame frame = new Frame("Counter-Browser");
-		frame.add(new Button("inc"));
-		frame.add(new Button("dec"));
-		
-		frame.addWindowListener(this);
-		frame.setVisible(true);*/
-		//while(true) {
-			//System.out.println(counter);
-			/*Scanner in = new Scanner(System.in);
-			int i = in.nextInt();
-			try {
-				counter.set(i);
-			}
-			catch(RemoteException e) {
-				System.out.println("RemoteException while setting data!: " + e.getMessage());
-			}*/
-		//}
 	}
 	
 	private Counter counter;
